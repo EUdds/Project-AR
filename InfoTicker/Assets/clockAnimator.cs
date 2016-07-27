0eb45e497b2036cf7ed083269e3c994686a43b4f
EUdds <coolspot1000@gmail.com>
2016-07-27 14:52:16 -0500

    Add Unity Files

---
+++ InfoTicker/Assets/clockAnimator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+public class clockAnimator : MonoBehaviour
+{
+    public Transform Hours, Minutes, Seconds;
+    public bool analog;
+    private const float
+        hoursToDegrees = 360f / 12f,
+        minutesToDegrees = 360f / 60f,
+        secondsToDegrees = 360f / 60f;
+
+
+
+    void Update()
+    {
+        if (analog)
+        {
+            TimeSpan timespan = DateTime.Now.TimeOfDay;
+            Hours.localRotation =
+                Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hoursToDegrees);
+            Minutes.localRotation =
+                Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -minutesToDegrees);
+            Seconds.localRotation =
+                Quaternion.Euler(0f, 0f, (float)timespan.TotalSeconds * -secondsToDegrees);
+            //Do Nothing
+        }
+        else
+        {
+
+
+            DateTime time = DateTime.Now;
+            Hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+            Minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
+            Seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+        }
+    }
+}
